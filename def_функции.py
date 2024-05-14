1b490ef78a3a61579b7be78135ed9b8a31ed4d10
NikitaGudinHomePC <nikitagudin.ads@gmail.com>
2024-05-14 21:25:13 +0300

    def

---
+++ def_функции.py
@@ -0,0 +1,52 @@
+def sum(a, b):
+    c = a + b
+    return c
+
+print(sum(5, 3))
+
+
+# самая короткая именованная функция
+def my_fn():
+    pass
+
+print(my_fn())
+
+
+# тут такой прикол что функция не видит внешний hello
+hello = 12
+def ad(ar):
+    ar = 1
+    hello = ar
+    print(hello)
+
+
+ad(13)
+print(hello)
+
+
+# Если передать в функцию внешний изменяемый объект
+# то его можно изменить (вау)
+dictic = {
+    'a': 1,
+}
+
+def dictos(dict):
+    dict['a'] += 1
+    return dict
+
+
+print(dictos(dictic))
+print(dictic)
+# Внутри функции не рекомендуется изменять внешние объекты
+
+
+# Чтобы избежать изменения объекта можно воспользовать копированием
+# как до функции так и внутри неё
+
+def dictos2(dict):
+    dict = dict.copy()
+    dict['a'] += 2
+    return dict
+
+print(dictos2(dictic))
+print(dictic)
--- изменение_объектов.py
+++ изменение_объектов.py
@@ -7,3 +7,29 @@ b = 10
 print(id(a))
 print(id(b))
 
+# Если надо избежать изменения всех копий объекта
+# То копии можно создавать с помощью а = объект.copy()
+# при этом вложенные объекты сохранят свои ссылки
+
+info = {
+    'name': 'Nikita',
+    'is_instructor': True,
+    'reviews': []
+}
+
+info_copy = info.copy()
+
+info_copy['reviews'].append('Great course!')
+
+print(info)
+print(info_copy)
+
+# Глубокое копирование
+from copy import deepcopy
+
+info_deepcopy = deepcopy(info)
+info_deepcopy['reviews'].append('fsdfsd')
+
+print(info)
+print(info_deepcopy)
+
