64d4aac2ea0dcce71a8b63da4f3ed08921dc3289
Nikita Gudin <nikitagudin.ads@gmail.com>
2024-05-17 17:28:52 +0300

    def arguments

--- def_аргументы.py
+++ def_аргументы.py
@@ -36,3 +36,24 @@ print(get_post_info(name="Nikita", posts_qty=25))
 
 
 
+# дз
+
+def merge_list_to_dict(a, b):
+    merged = zip(a, b)
+    itog = dict(merged)
+    return itog
+
+
+al = ['nikita', 'hello', 'au']
+bl = [123, 15, 5]
+
+print(merge_list_to_dict(a=al, b=bl))
+print(merge_list_to_dict(al, b=bl))
+
+# дз2
+
+def update_car_info(**person):
+    person['is_available'] = True
+    return person
+
+print(update_car_info(brand='Suzuki', price=300000))
