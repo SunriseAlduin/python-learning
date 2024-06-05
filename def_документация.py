ddcfed17fea68463abb6676db991490c31085ad2
Nikita Gudin <nikitagudin.ads@gmail.com>
2024-06-05 18:26:32 +0300

    документация

---
+++ def_документация.py
@@ -0,0 +1,16 @@
+def mult_by_factor(value, mult=1):
+    """_summary_
+
+    Args:
+        value (_type_): _description_
+        mult (int, optional): _description_. Defaults to 1.
+
+    Returns:
+        _type_: _description_
+    """
+    return value * mult
+
+mult_by_factor()
+
+
+
